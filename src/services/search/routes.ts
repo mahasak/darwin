5dffae5086bf72ea98846383eb43b616e6003ca6
Max Pijittum <mpijittum@fb.com>
2021-01-06 17:41:14 +0800

    Add sample chain filter

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,22 +1,23 @@
 import { Request, Response } from "express";
 import { getPlacesByName } from "./SearchController";
-
+import { checkSearchParams } from "../../middleware/checks";
 export default [
-  {
-    path: "/api/v1/search",
-    method: "get",
-    handler: [
-      async ({ query }: Request, res: Response) => {
-        const result = await getPlacesByName(query.q?.toString() ?? '');
-        res.status(200).send(result);
-      }
-    ]
-  },
-  {
-    path: "/",
-    method: "get",
-    handler: async (req: Request, res: Response) => {
-      res.send("Hello world!");
+    {
+        path: "/api/v1/search",
+        method: "get",
+        handler: [
+            checkSearchParams,
+            async ({ query }: Request, res: Response) => {
+                const result = await getPlacesByName(query.q?.toString() ?? '');
+                res.status(200).send(result);
+            }
+        ]
+    },
+    {
+        path: "/",
+        method: "get",
+        handler: async (req: Request, res: Response) => {
+            res.send("Hello world!");
+        }
     }
-  }
 ];
