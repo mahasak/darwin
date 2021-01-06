873890d8b7a60e5d438f23d439a8ad22cd74f6ed
Max Pijittum <mpijittum@fb.com>
2021-01-06 17:31:52 +0800

    Add sample API controller

--- src/services/search/routes.ts
+++ src/services/search/routes.ts
@@ -1,6 +1,17 @@
 import { Request, Response } from "express";
+import { getPlacesByName } from "./SearchController";
 
 export default [
+  {
+    path: "/api/v1/search",
+    method: "get",
+    handler: [
+      async ({ query }: Request, res: Response) => {
+        const result = await getPlacesByName(query.q?.toString() ?? '');
+        res.status(200).send(result);
+      }
+    ]
+  },
   {
     path: "/",
     method: "get",
