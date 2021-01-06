73991b471481eb9362bcc9904a542b32c25344e2
Max Pijittum <mpijittum@fb.com>
2021-01-07 00:59:54 +0800

    Add database infra code

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 import { init as initRedis } from './cache';
+import { init as initDb } from './db';
 
 const initDependencies = async () => {
   await initRedis();
+  await initDb();
 };
 
 export { initDependencies };
